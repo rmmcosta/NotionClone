522249ae52212616328d01b3ee60016cfecc6d39
rmmcosta <ricardocosta101085@gmail.com>
2023-12-22 09:36:03 +0000

    feat: home page without styles - ng version

--- notion-clone-ng/src/app/app.component.ts
+++ notion-clone-ng/src/app/app.component.ts
@@ -2,13 +2,21 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { TypewriterComponent } from './typewriter/typewriter.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
-    selector: 'app-root',
-    standalone: true,
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.scss',
-    imports: [CommonModule, RouterOutlet, TypewriterComponent]
+  selector: 'app-root',
+  standalone: true,
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    TypewriterComponent,
+    MatIconModule,
+    MatButtonModule,
+  ],
 })
 export class AppComponent {
   title = 'notion-clone';
