83c1060d81a7339ad248b5ddc367d861caa0c6e8
rmmcosta <ricardocosta101085@gmail.com>
2023-12-23 09:57:08 +0000

    feat: sign in and out with clerk for react version

--- notion-clone-react/src/App.tsx
+++ notion-clone-react/src/App.tsx
@@ -5,6 +5,13 @@ import Link from "@mui/material/Link";
 import Button from "@mui/material/Button";
 import { ArrowRight } from "@mui/icons-material";
 import { pink } from "@mui/material/colors";
+import {
+  SignOutButton,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/clerk-react";
 
 function App() {
   const getStartedColor = pink["A200"];
@@ -32,6 +39,23 @@ function App() {
             </Button>
           </Link>
         </div>
+      </div>
+      <div className="h-screen">
+        <UserButton afterSignOutUrl="/" />
+        <SignedOut>
+          <SignInButton />
+          <p>
+            This content is public. Only signed out users can see the
+            SignInButton above this text.
+          </p>
+        </SignedOut>
+        <SignedIn>
+          <SignOutButton />
+          <p>
+            This content is private. Only signed in users can see the
+            SignOutButton above this text.
+          </p>
+        </SignedIn>
       </div>
     </div>
   );
