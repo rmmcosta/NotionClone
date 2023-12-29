bcdff53b6f9f5382522bd298e7c01d5ce1a7a376
rmmcosta <ricardocosta101085@gmail.com>
2023-12-29 14:41:07 +0000

    feat: sekelton loading

--- notion-clone-react/src/layouts/dashboard-layout.tsx
+++ notion-clone-react/src/layouts/dashboard-layout.tsx
@@ -1,24 +1,28 @@
-import * as React from 'react'
-import { useAuth } from "@clerk/clerk-react"
-import { Outlet, useNavigate } from "react-router-dom"
- 
+import * as React from "react";
+import { useAuth } from "@clerk/clerk-react";
+import { Outlet, useNavigate } from "react-router-dom";
+
 export default function DashboardLayout() {
-    const { userId, isLoaded } = useAuth()
-    const navigate = useNavigate()
-    const [isRedirecting, setIsRedirecting] = React.useState(false);
- 
-    //console.log('test', userId)
- 
-    React.useEffect(() => {
-        if (isLoaded && !userId) {
-            setIsRedirecting(true);
-            navigate("/sign-in")
-        }
-    }, [navigate, userId, isLoaded])
- 
-    if (!isLoaded || isRedirecting) return <div>Loading...</div>
- 
+  const { userId, isLoaded } = useAuth();
+  const navigate = useNavigate();
+  const [isRedirecting, setIsRedirecting] = React.useState(false);
+
+  //console.log('test', userId)
+
+  React.useEffect(() => {
+    if (isLoaded && !userId) {
+      setIsRedirecting(true);
+      navigate("/sign-in");
+    }
+  }, [navigate, userId, isLoaded]);
+
+  if (!isLoaded || isRedirecting)
     return (
-        <Outlet />
-    )
+      <div className="skeleton-loader">
+        <div className="skeleton-header"></div>
+        <div className="skeleton-content"></div>
+      </div>
+    );
+
+  return <Outlet />;
 }
